16782d35eb8099e5cb57c588b209c878aaba9fc6
kellsonphilips <kellsonphilips@gmail.com>
2025-06-30 23:55:11 +0100

    CSP bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,7 @@ const nextConfig = {
           { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
           { key: 'Content-Security-Policy', value: isDev
             ? "default-src 'self'; img-src 'self' https://images.unsplash.com data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; font-src 'self';"
-            : "default-src 'self'; img-src 'self' https://images.unsplash.com data:; script-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self';"
+            : "default-src 'self'; img-src 'self' https://images.unsplash.com data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; font-src 'self';"
           },
         ],
       },
